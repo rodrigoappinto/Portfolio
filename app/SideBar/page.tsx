0bcbd5da57faf958d5bd90f4f58d67d8447c5bc6
Rodrigo Pinto <rodrigoappinto@Rodrigos-MBP.fritz.box>
2024-02-16 20:25:26 +0100

    Changed Size of DragMe SVG

--- app/SideBar/page.tsx
+++ app/SideBar/page.tsx
@@ -12,7 +12,7 @@ export default function SideBar() {
         <div className="h-screen flex sticky top-0 my-2">
             <div className="w-1/8 text-[#726455]">
                 <Link href="/" >
-                <div onClick={() => router.refresh()} className="w-full h-24 mt-24 flex text-center items-center justify-center bg-[#F3EEEA] ">
+                <div onClick={() => router.refresh()}className="w-full h-24 mt-24 flex text-center items-center justify-center bg-[#F3EEEA] ">
                     <div className="w-1/3 text-left">
                         Pinto Digital Journal
                     </div>
@@ -23,7 +23,7 @@ export default function SideBar() {
                 </Link>
                 <div className="h-[calc(100%-17rem)] w-full flex flex-col justify-center items-center gap-4">
                     <Link href="/Portfolio">
-                        <SideBarButton name="Portfolio"/>
+                        <SideBarButton name="portfolio"/>
                     </Link>
                     <Link href="/About">
                         <SideBarButton name="About Me"/>
