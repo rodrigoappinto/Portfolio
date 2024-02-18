d435ee556f79fc8a5f2210c69bdeff63fd29c1a3
Rodrigo Pinto <rodrigoappinto@Rodrigos-MBP.fritz.box>
2024-02-18 18:47:35 +0100

    Change top navbar behaviour

--- app/SmallSideBar/page.tsx
+++ app/SmallSideBar/page.tsx
@@ -11,7 +11,7 @@ export default function SideBar() {
 
     return (
         <div>
-            <div className="z-999 sticky h-9 w-full flex items-center justify-center gap-x-16 sm:hidden bg-[#F3EEEA]" >
+            <div className="z-999 fixed h-9 w-full flex items-center justify-center gap-x-16 sm:hidden bg-[#F3EEEA]" >
                 <Link href="/Portfolio">
                     <SideBarButton name="PoRtFoLio"/>
                 </Link>
