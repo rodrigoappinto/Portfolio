3d45219beb3edbbced36c15d30b0eefb99b919f3
Rodrigo Pinto <rodrigoalexandrepereiraepinto@gmail.com>
2024-11-03 22:06:40 +0000

    Fixed Resolution M1 Pro 14 inches

--- app/Components/PhotoPreview.tsx
+++ app/Components/PhotoPreview.tsx
@@ -1,7 +1,7 @@
 export default function PorfolioItem(props: any) {
   return (
       <img
-        className="w-full lg:max-w-[20rem] 2xl:max-w-[25rem] 3xl:min-w-[46rem] h-auto aspect-[3/2] object-cover duration-200 hover:scale-[1.01] hover:opacity-90 hover:duration-200"
+        className="w-full lg:max-w-[20rem] 2xl:max-w-[25rem] 3xl:min-w-[40rem] h-auto aspect-[3/2] object-cover duration-200 hover:scale-[1.01] hover:opacity-90 hover:duration-200"
         src={props.url}
       />
   );
