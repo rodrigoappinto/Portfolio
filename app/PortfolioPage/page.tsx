c888ef6196d0ae64e1a170b044feca123f93118c
Rodrigo Pinto <rodrigoalexandrepereiraepinto@gmail.com>
2024-12-04 00:17:10 +0000

    Fixed problem of not showing images when added to firestore

--- app/PortfolioPage/page.tsx
+++ app/PortfolioPage/page.tsx
@@ -1,22 +1,21 @@
 import { db } from "../db/firebase";
 import Portfolio from "../Components/Portfolio";
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, query } from 'firebase/firestore';
 
 export default async function PortfolioPage() {
   let data = [];
 
   async function fetchData() {
-    const querySnapshot = await getDocs(collection(db, 'Portfolio'));
+    const q = query(collection(db, 'Portfolio'));
+    const querySnapshot = await getDocs(q);
     const dataArray = querySnapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
     return dataArray.sort((a, b) => a.id > b.id ? 1 : -1);
-  };
-
+  }
 
   data = await fetchData();
-
   return (
     <div className="h-full w-full">
       <div className="mt-10 flex flex-wrap items-center justify-center px-2 sm:mt-4">
