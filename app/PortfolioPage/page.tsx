95430b00209049d2b14222c83d00ecf66e107446
Rodrigo Pinto <rodrigoalexandrepereiraepinto@gmail.com>
2024-12-04 00:43:48 +0000

    Fixing caching problem #2

--- app/PortfolioPage/page.tsx
+++ app/PortfolioPage/page.tsx
@@ -1,21 +1,22 @@
 import { db } from "../db/firebase";
 import Portfolio from "../Components/Portfolio";
-import { collection, getDocs, query } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 
+export const dynamic = 'force-dynamic';
 export default async function PortfolioPage() {
   let data = [];
 
   async function fetchData() {
-    const q = query(collection(db, 'Portfolio'));
-    const querySnapshot = await getDocs(q);
+    const querySnapshot = await getDocs(collection(db, 'Portfolio'));
     const dataArray = querySnapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
     return dataArray.sort((a, b) => a.id > b.id ? 1 : -1);
-  }
+  };
 
   data = await fetchData();
+  console.log(data)
   return (
     <div className="h-full w-full">
       <div className="mt-10 flex flex-wrap items-center justify-center px-2 sm:mt-4">
