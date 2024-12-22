7f948106c0594b9e58a5fd0d75f71b652b603471
Rodrigo Pinto <rodrigoalexandrepereiraepinto@gmail.com>
2024-12-22 20:52:26 +0000

    added dynamic page to config all pages

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
  
+export const dynamic = 'force-dynamic';
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
